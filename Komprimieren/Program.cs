d3ebc8316f40511f899596b55bb62af018ad172e
Tobias <73227066+PaperTobi@users.noreply.github.com>
2024-12-04 09:27:17 +0100

    Komprimieren created

---
+++ Komprimieren/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+
+class BinaryCompress
+{
+    static void Main()
+    {
+        string s_Datei = "Datei.txt";
+        string s_DateiKomprimiert = "komprimierteDatei.txt";
+        DateiErstellen(s_Datei);
+        string content = LesenUndKonvertieren(s_Datei);
+        string komprimierterInhalt = Komprimieren(content);
+        DateiSpeichern(s_DateiKomprimiert, komprimierterInhalt);
+        Console.WriteLine("Datei komprimiert und gespeichert als " + s_DateiKomprimiert);
+    }
+
+    static void DateiErstellen(string s_Datei)
+    {
+        string s_Text = "Hallo World!!!!";
+        File.Delete(s_Datei);
+        File.Create(s_Datei).Close();
+        Console.WriteLine("Datei erstellt");
+        StreamWriter o_SW = new StreamWriter(s_Datei);
+        o_SW.WriteLine(s_Text);
+        o_SW.Close();
+        Console.WriteLine("Sollte geschrieben sein lol");
+    }
+
+    static string LesenUndKonvertieren(string s_Datei)
+    {
+        string content = "";
+        using (FileStream fs = new FileStream(s_Datei, FileMode.Open, FileAccess.Read))
+        {
+            int b;
+            while ((b = fs.ReadByte()) != -1)
+            {
+                content += (char)b;
+            }
+        }
+        return content;
+    }
+
+    static string Komprimieren(string content)
+    {
+        if (content.Length == 0) return "";
+
+        string komprimierterInhalt = "";
+        char currentChar = content[0];
+        int count = 1;
+
+        for (int i = 1; i < content.Length; i++)
+        {
+            if (content[i] == currentChar)
+            {
+                count++;
+            }
+            else
+            {
+                komprimierterInhalt += "{" + count + currentChar + "} ";
+                currentChar = content[i];
+                count = 1;
+            }
+        }
+        komprimierterInhalt += "{" + count + currentChar;
+        return komprimierterInhalt;
+    }
+
+    static void DateiSpeichern(string s_DateiKomprimiert, string inhalt)
+    {
+        StreamWriter o_SW = new StreamWriter(s_DateiKomprimiert);
+        o_SW.Write(inhalt);
+        o_SW.Close();
+    }
+}
