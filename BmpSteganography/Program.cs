b9b34497b81d13ff1cb48a6db47b35aa505917ef
Tobias <73227066+PaperTobi@users.noreply.github.com>
2024-12-18 09:49:21 +0100

    added copyreight

--- BmpSteganography/Program.cs
+++ BmpSteganography/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//Copyreight Thomas!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+using System;
 using System.IO;
 using System.IO.MemoryMappedFiles;
 using System.Collections;
